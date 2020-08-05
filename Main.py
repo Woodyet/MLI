cd71c7d94b4a8948665d32e24e6c833be64f09d9
Woody Bayliss <woody.bayliss@sky.com>
2020-08-05 14:41:31 +0100

    meh2

--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 
 
-model = tf.keras.models.load_model('MainModel.h5') 
+#model = tf.keras.models.load_model('MainModel.h5') 
 
-layersNweights = []
+#layersNweights = []
 
-for layer in model.layers:
-    layersNweights.append(layer.get_weights())
+#for layer in model.layers:
+#    layersNweights.append(layer.get_weights())
 
 
 
@@ -16,3 +16,13 @@ for layer in model.layers:
 ##############################
 ##############################
 ''' Testing Weights'''
+
+
+''' 10 inputs neurons to 20 fully connected neurons '''
+
+
+Weights = [[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],[-0.1, -0.2, -0.3, -0.4, -0.5, -0.6, -0.7, -0.8, -0.9, -1],[-4 ,-3, -2, -1, -0.5, 0.5, 1, 2, 3, 4],[0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1],
+           [-0.01, -0.02, -0.03, -0.04, -0.05, -0.06, -0.07, -0.08, -0.09, -0.1],[0.31, -0.32, 0.43, 0.24,-0.85, 0.56,-0.47, 0.18,-0.99,-0.91],[0.31,-0.32, 0.43, 0.24,- 0.85, 0.56, 0.47, -0.18, 0.99,- 0.91],
+           [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],[-0.1, -0.2, -0.3, -0.4, -0.5, -0.6, -0.7, -0.8, -0.9, -1],[-4 ,-3, -2, -1, -0.5, 0.5, 1, 2, 3, 4]]
+
+print(len(Weights))
